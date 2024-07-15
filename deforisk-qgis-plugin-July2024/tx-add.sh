4a22bba5a9086eaf9a815babb597cd6b3884cdb3
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2024-07-15 08:24:52 -0500

    New presentations

---
+++ deforisk-qgis-plugin-July2024/tx-add.sh
@@ -0,0 +1,8 @@
+tx add \
+    --file-filter="po4a/deforisk-qgis-plugin.<lang>.po" \
+    --type=PO \
+    --organization=Cirad \
+    --project=oral-presentations \
+    --resource=deforisk-qgis-plugin \
+    --resource-name="Presentation deforisk-qgis-plugin" \
+    po4a/deforisk-qgis-plugin.pot
